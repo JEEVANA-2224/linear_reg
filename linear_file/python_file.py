ea7c9a98df4a4ab5f79379d3558365d3fa60eca9
JEEVANA-2224 <jeevanatamil22@gmail.com>
2025-12-25 08:41:19 +0530

    added

--- linear_file/python_file.py
+++ linear_file/python_file.py
@@ -16,8 +16,12 @@ st.write("Predict the final score of a student based on their performance featur
 
 # ----------------- Load Model and Scaler -----------------
 @st.cache_resource
+@st.cache_resource
 def load_model():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    st.error(f"BASE_DIR = {BASE_DIR}")
+    st.error(f"FILES = {os.listdir(BASE_DIR)}")
 
     model_path = os.path.join(BASE_DIR, "student_model.pkl")
     scaler_path = os.path.join(BASE_DIR, "student_scaler.pkl")
